6ed05362c727e651d9d4b886651e6fb2224ca8c7
Sebastian Riquelme <sebarsalbo@gmail.com>
2023-06-01 13:05:11 -0400

    Ayudantía 10 login funcional

--- codigo_ayudantía10/public/js/index.js
+++ codigo_ayudantía10/public/js/index.js
@@ -3,10 +3,20 @@ window.onload = function () {
 		.then((response) => response.json())
 		.then((data) => {
 			const menuItems = document.querySelectorAll(".nav-item");
+			const loginButton = document.querySelector('a[href="/login"]');
+			const registerButton = document.querySelector(
+				'a[href="/register"]'
+			);
 			if (data.authenticated) {
 				menuItems.forEach((item) => (item.style.display = "block"));
+				// Si el usuario está autenticado, ocultar los botones de inicio de sesión y registro
+				loginButton.classList.add("d-none");
+				registerButton.classList.add("d-none");
 			} else {
 				menuItems.forEach((item) => (item.style.display = "none"));
+				// Si el usuario no está autenticado, mostrar los botones de inicio de sesión y registro
+				loginButton.classList.remove("d-none");
+				registerButton.classList.remove("d-none");
 			}
 		});
 };
